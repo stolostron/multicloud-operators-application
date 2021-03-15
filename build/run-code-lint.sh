37e29bce65b4f2f5109d6407cc533e948fa147e6
Mike Ng <ming@redhat.com>
2021-03-15 12:44:14 -0400

    use dedicated lint script
    Signed-off-by: Mike Ng <ming@redhat.com>

--- build/run-code-lint.sh
+++ build/run-code-lint.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-set -o errexi
-set -o nounset
-set -o pipefail
-set -o xtrace
+###############################################################################
+# Copyright Contributors to the Open Cluster Management project
+###############################################################################
 
-# Prepare lint tools
-# Install hadolint
-HADOLINT_PATH="${HOME}"/hadolint
-mkdir -p "${HADOLINT_PATH}"
-wget -P "${HADOLINT_PATH}" https://github.com/hadolint/hadolint/releases/download/v1.17.5/hadolint-Linux-x86_64
-mv "${HADOLINT_PATH}"/hadolint-Linux-x86_64 "${HADOLINT_PATH}"/hadolint
-chmod +x "${HADOLINT_PATH}"/hadolint
-export PATH="${HADOLINT_PATH}":"${PATH}"
+set -x
+set -eo pipefail
 
-# Install yamllint
-pip install --user yamllint
+GOLANGCI_LINT_VERSION="1.24.0"
+GOLANGCI_LINT_CACHE=/tmp/golangci-cache
+GOOS=$(go env GOOS)
+GOPATH=$(go env GOPATH)
+export GOFLAGS=""
 
-# Install markdown lint
-gem install mdl
-gem install awesome_bot
+if ! which golangci-lint > /dev/null; then
+    mkdir -p "${GOPATH}/bin"
+    echo "${PATH}" | grep -q "${GOPATH}/bin"
+    IN_PATH=$?
+    if [ $IN_PATH != 0 ]; then
+        echo "${GOPATH}/bin not in $$PATH"
+        exit 1
+    fi
+    DOWNLOAD_URL="https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCI_LINT_VERSION}/golangci-lint-${GOLANGCI_LINT_VERSION}-${GOOS}-amd64.tar.gz"
+    curl -sfL "${DOWNLOAD_URL}" | tar -C "${GOPATH}/bin" -zx --strip-components=1 "golangci-lint-${GOLANGCI_LINT_VERSION}-${GOOS}-amd64/golangci-lint"
+fi
 
-# Install golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.24.0
-
-# Start lint task
-make lint
+echo 'Running linting tool ...'
+$(GOLANGCI_LINT_CACHE=${GOLANGCI_LINT_CACHE} golangci-lint run -c common/config/.golangci.yml)
+echo '##### lint-check #### Success'

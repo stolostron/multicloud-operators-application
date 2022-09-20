94ebfff509783fa0bdabb4a18ecfccdbfaaa9af9
Jonathan Marcantonio <jmarcant@redhat.com>
2022-09-20 12:39:45 -0400

    Change lease-duration to leader-election-lease-duration (#166)
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- cmd/manager/exec/manager.go
+++ cmd/manager/exec/manager.go
@@ -82,7 +82,7 @@ func RunManager() {
 		klog.Info("LeaderElection disabled as not running in a cluster")
 	}
 
-	leaseDuration := time.Duration(options.LeaseDurationSeconds) * time.Second
+	leaseDuration := time.Duration(options.LeaderElectionLeaseDurationSeconds) * time.Second
 	renewDeadline := time.Duration(options.RenewDeadlineSeconds) * time.Second
 	retryPeriod := time.Duration(options.RetryPeriodSeconds) * time.Second
 	// Create a new Cmd to provide shared dependencies and start components
